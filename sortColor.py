5aa469a7b3dbd67be4f5ff0c48fb438634d7ee8f
EdlawitG <88848586+EdlawitG@users.noreply.github.com>
2022-10-08 00:06:09 +0300

    SortingColor

---
+++ sortColor.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        for i in range(length-1):
+            for j in range(i+1,length):
+                if nums[i] > nums[j]:
+                    nums[i],nums[j] = nums[j],nums[i]
+        return nums
